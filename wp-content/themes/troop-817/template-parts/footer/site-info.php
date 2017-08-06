633150c7718153dcb9a5f21c8089a286b83e3208
Jim Rhines <jrhines@laughlin.com>
2017-08-06 18:46:26 -0500

    Initial upload of Troop 817.

---
+++ wp-content/themes/troop-817/template-parts/footer/site-info.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Troop_817
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	&#169;2017 Boy Scouts of America Troop 817. Grafton, WI 53024 USA. All Rights Reserved. 
+	<a href="/privacy-policy">Privacy Policy</a> | <a href="/site-map">Site Map</a>
+</div><!-- .site-info -->
